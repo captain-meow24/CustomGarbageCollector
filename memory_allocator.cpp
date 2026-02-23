84120682c9bd4b5673c8ef34638b6ac5987b4419
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-02-24 02:57:53 +0530

    writjing a memory alllocatr

--- memory_allocator.cpp
+++ memory_allocator.cpp
@@ -1,3 +1,10 @@
 //
 // Created by kanishka on 23/2/26.
 //
+#include "memory_allocator.h"
+
+void *heap_alloc(size_t size) {
+    assert(heap_size + size <= CAPACITY);
+    void *result = heap_size + size;
+
+}
--- memory_allocator.h
+++ memory_allocator.h
@@ -5,12 +5,17 @@
 #ifndef MEMORY_ALLOCATOR_H
 #define MEMORY_ALLOCATOR_H
 
+#include<iostream>
+#include<cassert>
+
 #define CAPACITY 64000
 
 char heap[CAPACITY]{};
+size_t heap_size = 0;
 
-void heap_free(void *ptr);
+
 void *heap_alloc(size_t size);
+void heap_free(void *ptr);
 
 
 #endif //MEMORY_ALLOCATOR_H
