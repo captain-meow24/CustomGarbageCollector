6f0f009bf2db69dcc75b8bc3f2a7d805d0353bc4
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-03-24 11:51:29 +0530

    bug fix

--- Garbage_collector.cpp
+++ Garbage_collector.cpp
@@ -4,9 +4,22 @@
 
 #include "Garbage_collector.h"
 
-void get_stack(void *stack_base_addr, size_t &size) {
-    pthread_attr_t attr;
-    pthread_getattr_np(pthread_self(), &attr);
+void get_stack() {
+    size_t size;
+    char* stack_base_addr;  //this gives the top of the stack memory, which is mostly unused
+    pthread_attr_t attr;  //attr will store all the meta-data about the running thread, i.e main thread
+    pthread_getattr_np(pthread_self(), &attr);    //pthread_self(0 will call the current thread
+    pthread_attr_getstack(&attr, (void**)&stack_base_addr,&size);
+    stack_top = stack_base_addr + size;
+    pthread_attr_destroy(&attr);   //destroying attr to avoid resources leak
+    asm volatile("mov %%rsp, %0" : "=r"(current_top));
+}
 
-    pthread_attr_getstack(&attr, &stack_base_addr, &size);
+void scan_stack() {
+
+    char* temp = stack_top;
+    while (temp<stack_top) {
+
+        temp += 8;
+    }
 }
--- Garbage_collector.h
+++ Garbage_collector.h
@@ -1,14 +1,20 @@
 //
 // Created by kanishka on 23/3/26.
 //
-#include<stdio.h>
-#include <pthread.h>
 
 #ifndef GARBAGE_COLLECTOR_H
 #define GARBAGE_COLLECTOR_H
 
-void get_stack(void* stack_base_addr, size_t &size);
+#include <pthread.h>
+#include "memory_allocator.h"
+
+char *stack_top;   //this is where the stack memory starts storing, this is the highest address as stack grows towards lower memory address
+void* current_top;
+
+void get_stack();  //gets size and bottom address of the stack
 void mark();
 void sweep();
+void scan_stack();
+void scan_heap();
 
 #endif //GARBAGE_COLLECTOR_H
