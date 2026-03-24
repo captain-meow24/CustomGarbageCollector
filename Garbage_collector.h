33c51d7ea6b27a8869e81cc28ea8bc5ed4fa346f
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-03-25 00:24:32 +0530

    added functions

--- Garbage_collector.h
+++ Garbage_collector.h
@@ -7,14 +7,16 @@
 
 #include <pthread.h>
 #include "memory_allocator.h"
+#include <cstdint>
 
 char *stack_top;   //this is where the stack memory starts storing, this is the highest address as stack grows towards lower memory address
-void* current_top;
+void* current_base;   //this will store the starting address of stack/highest address
 
 void get_stack();  //gets size and bottom address of the stack
 void mark();
 void sweep();
 void scan_stack();
 void scan_heap();
+void compare_heap(uint64_t temp);
 
 #endif //GARBAGE_COLLECTOR_H
