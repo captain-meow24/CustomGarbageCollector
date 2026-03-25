c8f3734a05e6d92ed80dbb71363d6cd727224960
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-03-25 11:03:31 +0530

    made a function to scan process stack

--- Garbage_collector.cpp
+++ Garbage_collector.cpp
@@ -7,22 +7,22 @@
 
 void get_stack() {
     size_t size;
-    char* stack_base_addr;  //this gives the top of the stack memory, which is mostly unused
+    char* stack_low;  //this gives the top of the stack memory, which is mostly unused
     pthread_attr_t attr;  //attr will store all the meta-data about the running thread, i.e main thread
     pthread_getattr_np(pthread_self(), &attr);    //pthread_self(0 will call the current thread
-    pthread_attr_getstack(&attr, (void**)&stack_base_addr,&size);   //void** means it points tos something unknown, it can hold any type
-    stack_top = stack_base_addr + size;
+    pthread_attr_getstack(&attr, (void**)&stack_low,&size);   //void** means it points tos something unknown, it can hold any type
+    stack_high = stack_low + size;
     pthread_attr_destroy(&attr);   //destroying attr to avoid resources leak
     asm volatile("mov %%rsp, %0" : "=r"(current_base));
 }
 
 void scan_stack() {
 
-    char* temp = stack_top;
-    while (temp< current_base) {
+    char* temp = (char*)current_base;
+    while (temp< stack_high) {
         uint64_t value = *(uint64_t*)temp;
-
-        temp += 8;
+        mark_meta(value);
+        temp += 16;   //pointers are 16 byte aligned in x86 64
     }
 }
 
--- Garbage_collector.h
+++ Garbage_collector.h
@@ -9,7 +9,7 @@
 #include "memory_allocator.h"
 #include <cstdint>
 
-char *stack_top;   //this is where the stack memory starts storing, this is the highest address as stack grows towards lower memory address
+char *stack_high;   //this is where the stack memory starts storing, this is the highest address as stack grows towards lower memory address
 void* current_base;   //this will store the starting address of stack/highest address
 
 void get_stack();  //gets size and bottom address of the stack
